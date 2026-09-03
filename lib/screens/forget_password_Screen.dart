34a2abbaf3838338e1ae7986a7c4f38d103af2c7
Mahmoud Hegazy <mahmoud.hegazy.dev@gmail.com>
2026-09-04 02:22:04 +0300

    Forgetpass (#12)
    * login_screen & register_screen & forget_password_Screen
    
    * Add login with google UI & language selector UI
    
    * add const
    
    ---------
    
    Co-authored-by: mohamed <msharawy986@gmail.com>

--- lib/screens/forget_password_Screen.dart
+++ lib/screens/forget_password_Screen.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
 import '../widgets/default_elevated_button.dart';
 import '../widgets/default_text_form_field.dart';
 
@@ -11,26 +9,25 @@ class ForgetPasswordScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Forget Password"), centerTitle: true),
-      body:SafeArea(
+      appBar: AppBar(title: const Text("Forget Password")),
+      body: SafeArea(
         child: Padding(
-          padding:  EdgeInsets.symmetric(horizontal: 16),
+          padding: const EdgeInsets.symmetric(horizontal: 16),
           child: SingleChildScrollView(
             child: Column(
               children: [
                 SvgPicture.asset(
                   "assets/images/Forgot password-bro 1.svg",
-                  height:  MediaQuery.sizeOf(context).height * .46,
+                  height: MediaQuery.sizeOf(context).height * .46,
                 ),
-                SizedBox(height: MediaQuery.sizeOf(context).height*.025,),
+                const SizedBox(height: 24),
                 DefaultTextFormField(
                   hintText: "Email",
                   prefixIconImageName: "email_icon",
                 ),
-                SizedBox(height: MediaQuery.sizeOf(context).height*.025,),
-            
-                DefaultElevatedButton(label: "Verify Email",onPressed: (){},)
-            
+                const SizedBox(height: 24),
+
+                DefaultElevatedButton(label: "Verify Email", onPressed: () {}),
               ],
             ),
           ),
