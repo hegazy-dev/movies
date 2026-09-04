b8865387d6f044eca71b6eb0335cb8f43a741a8e
nourshakra <nrshakra@gmail.com>
2026-09-04 08:55:26 +0200

    add widgets,splash screen and assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'onboarding/onboarding_screen.dart';
+//import 'onboarding/onboarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Movie Onboarding',
       theme: ThemeData.dark(),
-      home: const OnboardingScreen(),
+     // home: const OnboardingScreen(),
     );
   }
 }
