96f2fde11aca759247799df8b35762ef0a07b2cf
mohamed <msharawy986@gmail.com>
2026-09-09 19:06:09 +0300

    implement movies data

---
+++ lib/features/home/presentation/view_models.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/state/ui_state.dart';
+import '../../data/models/movie_model.dart';
+import '../../data/repositories/movie_repository.dart';
+
+class HomeViewModel extends ChangeNotifier {
+  final MovieRepository movieRepository;
+
+  // Use the shared UiState to hold the list of movies
+  // Assume UiState has factories like .initial(), .loading(), .success(), .empty(), .error()
+  UiState<List<MovieModel>> _moviesState = UiState.initial();
+
+  UiState<List<MovieModel>> get moviesState => _moviesState;
+
+  HomeViewModel({required this.movieRepository});
+
+  Future<void> fetchMovies() async {
+    _moviesState = UiState.loading();
+    notifyListeners();
+
+    try {
+      final movies = await movieRepository.getMovies();
+
+      if (movies.isEmpty) {
+        _moviesState = UiState.empty();
+      } else {
+        _moviesState = UiState.success(movies);
+      }
+    } catch (e) {
+      _moviesState = UiState.error(e.toString());
+    } finally {
+      notifyListeners();
+    }
+  }
+}
